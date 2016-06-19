a78b1dd3f43d10a470e6279f125dadfc4efbbb92
Satish Kris <skris232@gmail.com>
2016-06-19 12:03:54 +1000

    stock-tracker-base

--- app/myAccount/myAccount.js
+++ app/myAccount/myAccount.js
@@ -1,14 +1,21 @@
 'use strict';
 
-angular.module('stockTracker.myAccount', ['ngRoute'])
+angular.module('stockTracker.myAccount', ['ngRoute']).
+
+constant('APP_PROPS', {
+  'BALANCE' : 1000
+})
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/myAccount', {
     templateUrl: 'myAccount/myAccount.html',
-    controller: 'MyAccountCtrl'
+    controller: 'MyAccountCtrl',
+    controllerAs: 'vm'
   });
 }])
 
-.controller('MyAccountCtrl', [function() {
+.controller('MyAccountCtrl', ['APP_PROPS', function(APP_PROPS) {
+    var vm = this;
 
+    vm.balance = APP_PROPS.BALANCE;
 }]);
